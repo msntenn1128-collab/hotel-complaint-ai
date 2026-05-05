0dbd9affd584e166709b08b258b3180348cd1f58
msntenn1128-collab <msn.tenn.1128@gmail.com>
2026-05-05 20:53:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from openai import OpenAI
 import re
+from supabase import create_client
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 st.set_page_config(
     page_title="ホテル接遇AIアシスタント",
